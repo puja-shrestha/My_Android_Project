e5dbcc5f74453464d7187282c869c96472b7ea32
Puja shrestha <shresthaa.puza@gmail.com>
2017-12-06 11:25:04 +0545

    lession 02

--- UDEMY/CoderSwag/app/src/main/java/com/example/mypc/coderswag/Controller/MainActivity.kt
+++ UDEMY/CoderSwag/app/src/main/java/com/example/mypc/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.mypc.coderswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import com.example.mypc.coderswag.Model.Category
 import com.example.mypc.coderswag.R
+import com.example.mypc.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //initializing an array adapter
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+
+        //set a adapter for a list view itself
+        categoryListView.adapter = adapter
     }
 }
--- UDEMY/CoderSwag/app/src/main/java/com/example/mypc/coderswag/Model/Category.kt
+++ UDEMY/CoderSwag/app/src/main/java/com/example/mypc/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.mypc.coderswag.Model
 /**
  * Created by My PC on 12/5/2017.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
